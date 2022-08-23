b13c6f7015723e84673102873ec7adc906919ef4
bogdanaks <bogdanaks@bk.ru>
2022-08-24 01:02:40 +0300

    Change currency

--- sc/hardhat.config.ts
+++ sc/hardhat.config.ts
@@ -7,7 +7,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.9",
   gasReporter: {
     enabled: true,
-    currency: "USD",
+    currency: "RUB",
     coinmarketcap: "22f884de-2e05-4552-8402-bc45bb63d5ed",
     token: "ETH",
   },
